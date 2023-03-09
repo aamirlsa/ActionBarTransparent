f40ee90c771d0d4cc0ce10167fd0f4baff106994
aamirlsa <aamirmalik00116@gmail.com>
2023-03-09 11:41:48 +0530

    Initial commit
    status bar transparent
    
    first make a style theme
    set theme
    write java code

---
+++ app/src/main/java/com/jk/unfollowers/actionbartransparent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jk.unfollowers.actionbartransparent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // Code to run on KitKat (API 19) or later
+            // For example, you can set the status bar color to transparent
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+
+    }
+}
